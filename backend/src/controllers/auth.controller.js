afcb7b722afcb0cd92dbc90bdd3e4e43b180a281
artur.filippovskii <arturfilippovskij@gmail.com>
2025-04-24 17:40:41 +0300

    change sameSite for prod

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -23,7 +23,7 @@ export class AuthController {
       res.cookie('refreshToken', refreshToken, {
         httpOnly: true,
         secure: true,
-        sameSite: 'strict',
+        sameSite: 'none',
       });
 
       res.status(200).json({ accessToken, user });
